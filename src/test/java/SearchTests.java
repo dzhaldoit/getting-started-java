af56751a38a46765b906c5be505bf230948b69df
Dzhalal Atabaev <djaldoit@gmail.com>
2024-09-02 15:15:50 +0300

    eager static load

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -1,10 +1,16 @@
-import org.junit.jupiter.api.Test;
+  import com.codeborne.selenide.Configuration;
+  import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class SearchTests {
+
+    static {
+        Configuration.pageLoadStrategy = "eager";
+    }
+
     @Test
     void successfulSearchTest() {
         open("https://www.google.com/");
