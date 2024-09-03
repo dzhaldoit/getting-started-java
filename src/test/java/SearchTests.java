995734eb6c3556b132ce4c3908968bc3e96032b8
Dzhalal Atabaev <168621962+dzhaldoit@users.noreply.github.com>
2024-09-03 15:49:25 +0300

    Revert "delete eager static load"

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -6,7 +6,6 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class SearchTests {
-  
     static {
         Configuration.pageLoadStrategy = "eager";
     }
